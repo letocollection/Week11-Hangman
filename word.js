04ef268e97a7ab4a5f5a9c397b3497da7ba8b102
letocollection <leonard@letocollection.com>
2016-09-13 11:55:07 -0700

    random words done

--- word.js
+++ word.js
@@ -2,4 +2,26 @@
 
 //this should contain all the methods which will check the letters guessed versus the random word selected. 
 
-var game = require('./game.js');
+require('./game.js');
+
+
+// WordArray = [];
+// WordUArray = [];
+
+
+
+// PullWord();
+
+SetUnderline = function() {
+    PullWord();
+    console.log(this.word);
+
+//     for (i = 0; i < word.length; i++) {
+//         WordArray[i] = word.charAt(i);
+//         WordUArray[i] = "  ___  ";
+   };
+//     WordU = WordUArray.join("");
+//     console.log(word);
+// }
+
+SetUnderline();
